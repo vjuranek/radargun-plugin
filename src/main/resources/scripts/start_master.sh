0982a96b4cf6f59cda0bff5b3b0bd7edec548ce7
Vojtech Juranek <vjuranek@redhat.com>
2014-08-19 10:08:42 +0200

    Reformat start scripts

--- src/main/resources/scripts/start_master.sh
+++ src/main/resources/scripts/start_master.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-set -x
-
+# Jenkins RadarGun plugin
 # Script for launching RG master
 # See org.jenkinsci.plugins.radargun.script.ScriptSource#getMasterCmdLine for description of parameters
 # begin passed to this script and their order
 
+set -x
 $2 -c $3 -s $4
--- src/main/resources/scripts/start_slave.sh
+++ src/main/resources/scripts/start_slave.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -x
-
+# Jenkins RadarGun plugin
 # Script for launching RG master
-# See org.jenkinsci.plugins.radargun.script.ScriptSource#getMasterCmdLine for description of parameters
+# See org.jenkinsci.plugins.radargun.script.ScriptSource#getSlaveCmdLine for description of parameters
 # begin passed to this script and their order
 
+set -x
 $2
