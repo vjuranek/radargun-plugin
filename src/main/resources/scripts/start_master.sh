dfbad4815733391eef58ced18ff5ee0b1a8f0d86
Vojtech Juranek <vjuranek@redhat.com>
2015-07-02 22:37:43 +0200

    Remove misleading comments

--- src/main/resources/scripts/start_master.sh
+++ src/main/resources/scripts/start_master.sh
@@ -2,8 +2,6 @@
 
 # Jenkins RadarGun plugin
 # Script for launching RG master
-# See org.jenkinsci.plugins.radargun.script.ScriptSource#getMasterCmdLine for description of parameters
-# begin passed to this script and their order
 
 set -x
 ssh $1 $2 $3 $4 -c $5 -s $6 -J "$7"
--- src/main/resources/scripts/start_slave.sh
+++ src/main/resources/scripts/start_slave.sh
@@ -2,8 +2,6 @@
 
 # Jenkins RadarGun plugin
 # Script for launching RG master
-# See org.jenkinsci.plugins.radargun.script.ScriptSource#getSlaveCmdLine for description of parameters
-# begin passed to this script and their order
 
 set -x
 ssh $1 $2 $3 $4 -i $5 -J "$6"
