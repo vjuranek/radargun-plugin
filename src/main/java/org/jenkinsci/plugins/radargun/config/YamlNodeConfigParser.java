30e504540b36a6f0f0c58217817f2dc4fc3f9f53
Vojtech Juranek <vjuranek@redhat.com>
2015-07-17 10:55:09 +0200

    Allow various other YAML elements

--- src/main/java/org/jenkinsci/plugins/radargun/config/YamlNodeConfigParser.java
+++ src/main/java/org/jenkinsci/plugins/radargun/config/YamlNodeConfigParser.java
@@ -10,6 +10,8 @@ import org.yaml.snakeyaml.Yaml;
 
 public class YamlNodeConfigParser implements NodeConfigParser {
 
+    public static final String NODES_KEY = "nodes";
+    
     private final Yaml yaml;
 
     public YamlNodeConfigParser() {
@@ -19,18 +21,20 @@ public class YamlNodeConfigParser implements NodeConfigParser {
     @Override
     public NodeList parseNodeList(String nodesConfig) {
         @SuppressWarnings("unchecked")
-        List<Map<String, Object>> parsedConf = (List<Map<String, Object>>) yaml.load(nodesConfig);
-        if (parsedConf.size() < 2)
+        Map<String, Object> parsedConf = (Map<String, Object>) yaml.load(nodesConfig);
+        @SuppressWarnings("unchecked")
+        List<Map<String, Object>> nodesConf = (List<Map<String, Object>>) parsedConf.get(NODES_KEY);
+        if (nodesConf.size() < 2)
             throw new IllegalArgumentException(
                     "Wrong node configuration, at least two nodes (one master and one slave) required!");
 
-        Map<String, Object> masterConf = parsedConf.remove(0);
+        Map<String, Object> masterConf = nodesConf.remove(0);
         String masterHost = masterConf.keySet().iterator().next();
         @SuppressWarnings("unchecked")
         Node master = Node.parseNode(masterHost, (Map<String, Object>)masterConf.get(masterHost));
 
         List<Node> nodes = new LinkedList<Node>();
-        for (Map<String, Object> nodeConf : parsedConf) {
+        for (Map<String, Object> nodeConf : nodesConf) {
             String nodeHost = nodeConf.keySet().iterator().next();
             @SuppressWarnings("unchecked")
             Node node = Node.parseNode(nodeHost, (Map<String, Object>)nodeConf.get(nodeHost));
