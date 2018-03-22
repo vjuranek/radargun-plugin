2f59c6c6cdfdf9f4b93f94fb316402d176fdbb3d
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-03-22 10:58:20 +0100

    Resolve also java properties

--- src/main/java/org/jenkinsci/plugins/radargun/util/Resolver.java
+++ src/main/java/org/jenkinsci/plugins/radargun/util/Resolver.java
@@ -1,13 +1,16 @@
 package org.jenkinsci.plugins.radargun.util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import hudson.EnvVars;
 import hudson.Util;
 import hudson.model.AbstractBuild;
 import hudson.util.LogTaskListener;
 import hudson.util.VariableResolver;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Convenient class for resolving/expanding various variabales.
@@ -18,16 +21,21 @@ import java.util.logging.Logger;
 public class Resolver {
     
     private static AbstractBuild<?, ?> build;
+    private static EnvVars propertiesEnv;
     
     public static void init(final AbstractBuild<?, ?> build) {
         Resolver.build = build;
+        Properties props = System.getProperties();
+        Map<String, String> propMap = new HashMap<>(props.size());
+        props.entrySet().forEach(entry -> propMap.put((String)entry.getKey(), (String)entry.getValue()));
+        propertiesEnv = new EnvVars(propMap);
     }
     
     public static String doResolve(final String toResolve) throws IllegalStateException {
         if(build == null)
             throw new IllegalStateException("Variable resolver was not initialized!");
         
-        return buildVar(build, toResolve);
+        return javaProps(buildVar(build, toResolve));
     }
 
     public static String buildVar(final AbstractBuild<?, ?> build,final String toResolve) {
@@ -39,6 +47,19 @@ public class Resolver {
         try {
             EnvVars env = build.getEnvironment(new LogTaskListener(LOG, Level.INFO));
             resolved = env.expand(resolved);
+        } catch (Exception e) {
+            //TODO no-op?
+        }
+        return resolved;
+    }
+    
+    public static String javaProps(String toResolve) {
+        if(toResolve == null)
+            return null;
+        String resolved = toResolve;
+        
+        try {
+            resolved = propertiesEnv.expand(toResolve);
         } catch (Exception e) {
             //TODO no-op?
         }
